246d388bf744f25a6febe145862291ff46bf3a53
SirElms <empressran@gmail.com>
2016-12-25 23:47:47 +0800

    added the category.php template

---
+++ category.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+
+
+<div class="row">
+  <div class="col-md-8">
+
+
+<?php  if(have_posts() ): ?>
+<header class="text-center well">
+    <h2>  <?php single_cat_title('Category type: ', true); ?></h2>
+</header>
+
+
+
+        <?php while (have_posts() ):the_post(); ?>
+
+                  <?php  get_template_part('content','archive');  ?>
+
+        <?php endwhile?>
+
+        <div class="text-center">
+          <?php the_posts_pagination(array(
+              'mid_size' => 5,
+              'screen_reader_text' => 'Navigate here',
+
+          )) ?>
+        </div>
+
+<?php endif?>
+</div>
+
+<div class="col-md-4">
+  <?php get_sidebar(); ?>
+</div>
+
+
+
+
+</div>
+<?php get_footer(); ?>
